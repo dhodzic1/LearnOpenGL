77da4ddbb1091ae917bb3b1dd29009d2ce1fb4b0
Darian <darian2486@gmail.com>
2023-01-26 00:53:00 -0500

    updated GLSL as raw string literals

--- project/ogl.cpp
+++ project/ogl.cpp
@@ -7,28 +7,31 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // Vertex Shader GLSL
-const char *vertexShaderSource = "#version 460 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
+const char *vertexShaderSource = R"(
+#version 460 core
+layout (location = 0) in vec3 aPos;
+void main()
+{
+   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+};)";
 
 // Fragment 1 Shader GLSL
-const char *fragmentShaderSource = "#version 460 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\0";
+const char *fragmentShaderSource = R"(
+#version 460 core
+out vec4 FragColor;
+void main()
+{
+  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+};)";
 
 // Fragment 2 Shader GLSL
-const char *fragmentShaderSourceTwo = "#version 460 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"  FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
-"}\0";
+const char* fragmentShaderSourceTwo = R"(
+#version 460 core
+out vec4 FragColor;
+void main()
+{
+  FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);
+};)";
 
 
 int main()

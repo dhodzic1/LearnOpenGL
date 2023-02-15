b88902bdc105bbacd11b2d81781166981955900b
Darian <darian2486@gmail.com>
2023-02-14 20:17:36 -0500

    completed chapter 8 coordinate systems (added rotating cubes)

--- project/ogl.cpp
+++ project/ogl.cpp
@@ -98,6 +98,19 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     unsigned int VAO, VBO;  // for single do unsigned int VAO, VBO and reference in genVertexArrays and genBuffers as &VAO or &VBO
     // Generate vertex array objects and vertex buffer objects
     glGenVertexArrays(1, &VAO);
@@ -203,19 +216,31 @@ int main()
         glm::mat4 projection = glm::mat4(1.0f);
         model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH/ (float)SCR_HEIGHT, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(60.0f), (float)SCR_WIDTH/ (float)SCR_HEIGHT, 0.1f, 100.0f);
         // retrieve the matrix uniform locations
         unsigned int modelLocation = glGetUniformLocation(ourShader.ID, "model");
         unsigned int viewLocation = glGetUniformLocation(ourShader.ID, "view");
         unsigned int projLocation = glGetUniformLocation(ourShader.ID, "projection");
-        // pass them to the shaders (3 different ways)
-        glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+        // pass them to the shaders
         glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projLocation, 1, GL_FALSE, glm::value_ptr(projection));
 
         // Now render the triangles
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36); // draw cube
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            if (i % 2 == 0) {
+                model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+            }
+            else {
+                model = glm::rotate(model, (float)-glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+            }
+            glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+            // draw cubes
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         /*glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);*/
         //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // WIREFRAME, COMMENT FOR FILL
 

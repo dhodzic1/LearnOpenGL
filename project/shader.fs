3498d4a09f066cd93c0af434a9f55bde46a1dc8a
Darian <darian2486@gmail.com>
2023-02-28 17:43:16 -0500

    Implemented Phong, chapter 11 completed

--- project/shader.fs
+++ project/shader.fs
@@ -1,10 +1,33 @@
 #version 460 core
 out vec4 FragColor;
 
+in vec3 Normal;
+in vec3 FragPos; 
+
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 void main()
-{
-	FragColor = vec4(lightColor * objectColor, 1.0);
+{	
+	// ambient = (light color * ambientStrength) * objectColor
+	float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // diffuse 
+    vec3 norm = normalize(Normal);  // we always work with unit vectors, so DONT FORGET TO NORMALIZE VECTORS
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0); // diffuse impact on current fragment is dot product of normal vector and light direction vector
+    vec3 diffuse = diff * lightColor;   // multiply result with lightColor to get diffuse component
+
+    // specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm); 
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);   // raised to power of 32 for 'shininess' of highlight
+    vec3 specular = specularStrength * spec * lightColor;  
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+	FragColor = vec4(result, 1.0);
 }
--- project/shader.vs
+++ project/shader.vs
@@ -1,11 +1,22 @@
 #version 460 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+out vec3 Normal;
+out vec3 FragPos;  
+
 void main()
 {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    FragPos = vec3(model * vec4(aPos, 1.0));    // calculate fragment position = model matrix * vertexPosition
+
+    // The normal matrix is defined as 'the transpose of the inverse of the upper-left 3x3 part of the model matrix'
+    Normal = mat3(transpose(inverse(model))) * aNormal; // notice that we isolate the 3x3 part by casting to a mat3
+    // NOTE INVERSE of a matrix is costly, only done in book for learning purposes
+    // Usually, you would have to calculate the normal matrix on CPU and then send it to shaders via uniform just like the model matrix
+
+    gl_Position = projection * view * vec4(FragPos, 1.0);
 }

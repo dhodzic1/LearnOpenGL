1b87dc6767ea52948e7e92953f7fd85f10d3c955
Darian <darian2486@gmail.com>
2023-02-28 23:58:48 -0500

    chapter 13 completed lighting maps

--- project/shader.fs
+++ project/shader.fs
@@ -2,13 +2,13 @@
 out vec4 FragColor;
 
 in vec3 Normal;
-in vec3 FragPos; 
+in vec3 FragPos;
+in vec2 TexCoords;
 
 // In this struct we define a color vector for each of Phong's lighting components
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;
     float shininess;
 }; 
 
@@ -26,19 +26,19 @@ uniform vec3 viewPos;
 void main()
 {	
 	// ambient
-    vec3 ambient = light.ambient * material.ambient;
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
 
     // diffuse 
     vec3 norm = normalize(Normal);  // we always work with unit vectors, so DONT FORGET TO NORMALIZE VECTORS
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0); // diffuse impact on current fragment is dot product of normal vector and light direction vector
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);   // multiply result with lightColor to get diffuse component
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb; 
 
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm); 
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);   // raised to power of material.shininess for 'shininess' of highlight
-    vec3 specular = light.specular * (spec * material.specular);  
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
 
     vec3 result = ambient + diffuse + specular;
 	FragColor = vec4(result, 1.0);
--- project/shader.vs
+++ project/shader.vs
@@ -1,6 +1,7 @@
 #version 460 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -8,6 +9,7 @@ uniform mat4 projection;
 
 out vec3 Normal;
 out vec3 FragPos;  
+out vec2 TexCoords;
 
 void main()
 {
@@ -18,5 +20,6 @@ void main()
     // NOTE INVERSE of a matrix is costly, only done in book for learning purposes
     // Usually, you would have to calculate the normal matrix on CPU and then send it to shaders via uniform just like the model matrix
 
+    TexCoords = aTexCoords;
     gl_Position = projection * view * vec4(FragPos, 1.0);
 }
